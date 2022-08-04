a91fe7860a816fd07ef3377d4c332eb6a7bdc947
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-08-04 09:27:17 +0500

    Converted functional to class component and introduce error boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,24 @@
 import Header from '../components/header/Header';
 import Input from '../components/input/Input';
 import PostsList from '../components/post/PostsList';
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Input />
-      <scroll>
-        <PostsList />
-      </scroll>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Input />
+        <scroll>
+          <ErrorBoundry>
+            <PostsList />
+          </ErrorBoundry>
+        </scroll>
+      </div>
+    );
+  }
 }
 
 export default App;

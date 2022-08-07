88270a628db152b3eea629abd375e404664ce530
hadaw kakazai <hadsawkakazaihk@gmail.com>
2022-08-07 16:42:39 +0500

    complete post popup created...

--- src/components/post/PostsList.js
+++ src/components/post/PostsList.js
@@ -5,63 +5,65 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CompletePost from './CompletePost';
 
 function PostsList() {
     let description ="Some quick example text to build on the card title and make up thebulk of the card's content."
     return (
-        <Container  fluid={true} lg="auto" className="scrollable_div" >
-        <Row className="row_style">
-          <Col md={6} lg="auto">
-              <PostCard postNum="1" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col  md={6} lg="auto">
-              <PostCard postNum="2" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col  md={6} lg="auto">
-              <PostCard postNum="3" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col >
-          <Col  md={6} lg="auto">
-              <PostCard  postNum="4" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
+    //     <Container  fluid={true} lg="auto" className="scrollable_div" >
+    //     <Row className="row_style">
+    //       <Col md={6} lg="auto">
+    //           <PostCard postNum="1" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col  md={6} lg="auto">
+    //           <PostCard postNum="2" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col  md={6} lg="auto">
+    //           <PostCard postNum="3" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col >
+    //       <Col  md={6} lg="auto">
+    //           <PostCard  postNum="4" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
         
 
-        </Row>
-        <Row className="row_style">
-        <Col md={6} lg="auto">
-              <PostCard postNum="5" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col md={6} lg="auto">
-              <PostCard postNum="6" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col md={6} lg="auto">
-              <PostCard postNum="7" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col md={6} lg="auto">
-              <PostCard postNum="8" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
+    //     </Row>
+    //     <Row className="row_style">
+    //     <Col md={6} lg="auto">
+    //           <PostCard postNum="5" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col md={6} lg="auto">
+    //           <PostCard postNum="6" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col md={6} lg="auto">
+    //           <PostCard postNum="7" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col md={6} lg="auto">
+    //           <PostCard postNum="8" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
         
 
-        </Row >
+    //     </Row >
 
-        {/* <Row className="row_style">
-        <Col md={6} lg="auto">
-              <PostCard postNum="9" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col md={6} lg="auto">
-              <PostCard postNum="10" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col md={6} lg="auto">
-              <PostCard postNum="11" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
-          <Col md={6} lg="auto">
-              <PostCard postNum="12" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
-          </Col>
+    //     {/* <Row className="row_style">
+    //     <Col md={6} lg="auto">
+    //           <PostCard postNum="9" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col md={6} lg="auto">
+    //           <PostCard postNum="10" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col md={6} lg="auto">
+    //           <PostCard postNum="11" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
+    //       <Col md={6} lg="auto">
+    //           <PostCard postNum="12" cardname="Title will be here" cardDescrip={description} url_img="https://www.planetware.com/wpimages/2020/02/italy-in-pictures-beautiful-places-to-photograph-venice-canal.jpg"/>
+    //       </Col>
         
 
-        </Row>  */}
+    //     </Row>  */}
 
-      </Container>
+    //   </Container>
        
+    <CompletePost/>
     )
 }
 

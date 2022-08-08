dbeb90db5e19503f25b35778778d49f007bcf666
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-08-08 09:36:38 +0500

    Work on complete post view

--- src/components/post/PostCard.js
+++ src/components/post/PostCard.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useState } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import './post.css'
@@ -7,24 +9,49 @@ import CompletePost from './CompletePost';
 
 function PostCard({ post }) {
 
-  function readMore() {
-    return <CompletePost post={post} />
-  }
-  return (
-    <Card style={{ height: '35rem', width: '20rem', backgroundColor: '#F5F5DA', marginTop: '1rem', marginTop: '1rem' }}>
-      <span className='badge'>{post.id}</span>
+  const [isShown, setIsShown] = useState(true);
 
-      <Card.Body>
-        <img src={`https://robohash.org/${post.id}?20x20`} width='100px' height={'100px'} alt='Robot'></img>
-        <h5 style={{ marginTop: '1rem', fontSize: '2rem', textAlign: 'center' }}>{post.title}</h5>
-        <Card.Text style={{ height: '10rem' }}>
-          {post.body.substring(0, 200)}
-        </Card.Text>
-        {post.body.length > 200 && <Card.Link onClick={readMore} href="//"><hr></hr>Read More</Card.Link>}
+  const handleClick = event => {
+    // 👇️ toggle shown state
+    setIsShown(current => !current);
 
-      </Card.Body>
-    </Card>
-  )
+    // 👇️ or simply set it to true
+    // setIsShown(true);
+  };
+
+  if (isShown)
+    return (
+      <Card
+        id={post.id}
+        style={{
+          height: '35rem',
+          width: '20rem',
+          backgroundColor: '#F5F5DA',
+          marginTop: '1rem',
+          marginTop: '1rem'
+        }}>
+        <span
+          className='badge'>{post.id}</span>
+
+        <Card.Body>
+          <img
+            src={`https://robohash.org/${post.id}?20x20`}
+            width='100px'
+            height={'100px'}
+            alt='Post'></img>
+          <h5 style={{ marginTop: '1rem', fontSize: '2rem', textAlign: 'center' }}>{post.title}</h5>
+          <Card.Text style={{ height: '10rem' }}>
+            {post.body.substring(0, 200)}
+          </Card.Text>
+          {post.body.length > 200 && <Card.Link onClick={handleClick} href="//"><hr></hr>Read More</Card.Link>}
+
+        </Card.Body>
+      </Card>
+    )
+  else
+    return (
+      <CompletePost post={post} onClose={handleClick} />
+    )
 }
 
 export default PostCard

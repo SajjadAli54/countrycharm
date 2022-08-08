f50caf246a83bece4b3a773995f82c4c5f43a24a
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2022-08-08 09:49:47 +0500

    Modified Posts file

--- src/containers/posts.js
+++ src/containers/posts.js
@@ -3,7 +3,8 @@ const arr = [
         "userId": 1,
         "id": 1,
         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+        "body": `quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\n
+                reprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto`,
         "category": "Food"
     },
     {
